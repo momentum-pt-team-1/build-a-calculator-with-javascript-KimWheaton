147577c1d46c9dba1b43150135c985414380e830
Kim Wheaton <kwheatonart@gmail.com>
2021-06-28 12:04:58 -0400

    work in progress

---
+++ main.js
@@ -0,0 +1,25 @@
+import * as math from ("math.js")
+console.log(math.sqrt(4));
+
+// let calculator = document.querySelector("#display");
+// calculator.addEventListener("submit", function(event) {
+//   console.log("submit");
+// });
+
+// function add(a, b) {
+//     return a + b;
+//   };
+  
+//   function subtract(a, b) {
+//     return a - b;
+//   };
+  
+//   function multiply(a, b) {
+//     return a * b;
+//   };
+  
+//   function divide(a, b) {
+//     return a / b;
+//   };
+  
+  
