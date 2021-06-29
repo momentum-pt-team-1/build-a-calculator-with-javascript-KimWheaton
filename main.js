39c2dd308c761fcb2c3873619ca3b1ccd756c0b6
Kim Wheaton <kwheatonart@gmail.com>
2021-06-29 10:24:52 -0400

    updated math.js and main.js

--- main.js
+++ main.js
@@ -1,25 +1,3 @@
-import * as math from ("math.js")
-console.log(math.sqrt(4));
+console.log(math.add(1, 2));
 
-// let calculator = document.querySelector("#display");
-// calculator.addEventListener("submit", function(event) {
-//   console.log("submit");
-// });
 
-// function add(a, b) {
-//     return a + b;
-//   };
-  
-//   function subtract(a, b) {
-//     return a - b;
-//   };
-  
-//   function multiply(a, b) {
-//     return a * b;
-//   };
-  
-//   function divide(a, b) {
-//     return a / b;
-//   };
-  
-  
---
+++ scratch.js
@@ -0,0 +1,22 @@
+// let calculator = document.querySelector("#display");
+// calculator.addEventListener("submit", function(event) {
+//   console.log("submit");
+// });
+
+// function add(a, b) {
+//     return a + b;
+//   };
+  
+//   function subtract(a, b) {
+//     return a - b;
+//   };
+  
+//   function multiply(a, b) {
+//     return a * b;
+//   };
+  
+//   function divide(a, b) {
+//     return a / b;
+//   };
+  
+  
