39498eabaea5e7cf71d7f9c0a3deb95a7ff29795
Kim Wheaton <kwheatonart@gmail.com>
2021-06-30 16:21:29 -0400

    basic working calculator

--- main.js
+++ main.js
@@ -1,3 +1,60 @@
-console.log(math.add(1, 2));
+const calculator = document.querySelector("#calculator");
+const numberKeys = calculator.querySelectorAll(".number");
+const operatorKeys = calculator.querySelectorAll(".operator");
+const decimalKey = calculator.querySelector(".decimal");
+const equalKey = calculator.querySelector(".equal-sign");
+const allClearKey = calculator.querySelector(".all-clear");
+let display = document.querySelector("#calculator-screen");
+let expression = ""
+
+function enterNumber() {
+    for (let key of numberKeys) {
+        key.addEventListener('click', event => {
+            display.textContent += event.target.value;
+            });
+        };
+}
+
+function enterOperator() {
+    for (let key of operatorKeys) {
+        key.addEventListener('click', event => {
+            display.textContent += event.target.value;
+            });
+    };
+}   
+
+function enterDecimal() {
+    decimalKey.addEventListener('click', event => {
+        display.textContent += event.target.textContent;
+    });
+}
+
+function enterEqual() {
+    equalKey.addEventListener('click', event => {
+        let solution = math.evaluate(display.textContent)
+        display.textContent = solution
+    });
+}
+
+function enterAllCLear() {
+    allClearKey.addEventListener('click', event => {
+        display.textContent=""; 
+    });
+}
+
+function useCalculator() {
+enterNumber()
+enterOperator()
+enterDecimal()
+enterEqual()
+enterAllCLear()
+}
+
+useCalculator()
+
+
+
+
+
 
 
